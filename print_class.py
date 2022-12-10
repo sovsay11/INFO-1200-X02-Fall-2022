68216a761f2e54a986d5e78ca0ee3cbf1792b357
De-Clan-C <theduelistd8@gmail.com>
2022-12-10 09:24:27 -0700

    Basic program to print class names

---
+++ print_class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import AbigailSmith as ab, AshrafAbudu as ash, CalebJeffs as cal, ColeFelis as col, DanielWatson as dan, DavidOlson as dav, DeclanCottle as dec, DevinJohnson as dev, DongdiZhao as dong, \
+    ElmerColonMuniz as el, GarrettHarmon as gar, GregorioNunez as gre, KamryByers as kam, KelsieMcKay as kels, LaurynOlson as lauryn, MattThomon as mat, MorganBarlow as mor, NoahSay as noa, \
+        PatrickWong as pat, SpencerBarney as spen, SteveBarton as st
+
+print("The members of the fall 2022 programming I class are:")
+
+class_list = []
+
+
+class_list.append(ab.name)
+class_list.append(ash.name)
+class_list.append(cal.name)
+class_list.append(col.name)
+class_list.append(dan.name)
+class_list.append(dav.name)
+class_list.append(dec.name)
+class_list.append(dev.name)
+class_list.append(dong.name)
+class_list.append(el.name)
+class_list.append(gar.name)
+class_list.append(gre.name)
+class_list.append(kam.name)
+class_list.append(kels.name)
+class_list.append(lauryn.name)
+class_list.append(mat.name)
+class_list.append(mor.name)
+class_list.append(noa.name)
+class_list.append(pat.name)
+class_list.append(spen.name)
+class_list.append(st.name)
+
+print(class_list)
